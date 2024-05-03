dcbb44a6f0dc4618b47b1c8a5bd1db8b85ac2ee2
ShivamPansuriya <pansuriyashivam@gmail.com>
2024-05-03 18:40:06 +0530

    update

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
-import "Golang/advance"
+import (
+	"Golang/advance"
+)
 
 func main() {
 
-	print("hello world")
+	//print("hello world")
 	//advance.Structs()
 
 	//advance.Method()
@@ -14,4 +16,26 @@ func main() {
 	//advance.Error()
 
 	advance.Start()
+
+	//mapper := make(map[int]struct{})
+	//prepare(mapper)
+	//fmt.Println(fmt.Sprintf("value are %v", mapper))
+	//fmt.Println(mapper())
 }
+
+//func mapper() int {
+//	defer func() int {
+//		if err := recover(); err != nil {
+//			fmt.Println(err)
+//			return 3
+//		}
+//		return 0
+//
+//	}()
+//}
+
+//func prepare(mapper map[int]struct{}) {
+//	for i := 0; i <= 5; i++ {
+//		defer func() { mapper[i] = struct{}{} }()
+//	}
+//}
